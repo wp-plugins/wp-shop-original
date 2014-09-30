ed2dbaf447932897273a5d85496c9a94fb98a441
wpshop <wpshop@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 13:26:02 +0000

    version
    git-svn-id: https://plugins.svn.wordpress.org/wp-shop-original/trunk@999132 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-shop.php
+++ wp-shop.php
@@ -4,7 +4,7 @@
  Plugin URI: http://www.wp-shop.ru
  Description: Интернет-магазин для WordPress.
  Author: www.wp-shop.ru
- Version: 3.4.3.2
+ Version: 3.4.3.3
  Author URI: http://www.wp-shop.ru
  */
 
